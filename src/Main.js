0c18f5954acc0e5cf697499010962d2a7ab86d34
Anastasiia Smith <anastasiiasmithdev@gmail.com>
2022-02-17 20:05:56 -0500

    Added styles for Main, added state and event listener for get meme button

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,19 @@
+import React from "react"
+import memesData from "./memesData.js"
+
 function Main() {
+  const [memeImage, setMemeImage] = React.useState("");
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
   return (
     <main className="main">
       <input className="main__input" type="text" id="top-text" name="top-text"/>
       <input className="main__input" type="text" id="bottom-text" name="bottom-text"/>
-      <button className="main__button">Get a new meme image 🖼</button>
-      <img className="main__image" src="" alt="" />
+      <button onClick={getMemeImage} className="main__button">Get a new meme image 🖼</button>
+      <img className="main__image" src={memeImage} alt="" />
     </main>
   );
 }
