e671bb5337afa859b36ff0178ded5b5d8e06113a
Anastasiia Smith <anastasiiasmithdev@gmail.com>
2023-07-24 15:25:49 +0300

    fix: add media query for image and change readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Meme generator from scrimba.com react course
 
+Deploy: https://anastasiia-smith.github.io/meme-generator/
+
 A silly project where you can get the random meme from an API and add your text on top of it.
--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,6 @@ function Header() {
         <img className="header__image" src={trollFace} alt="Troll face" />
         <h1 className="header__title">Meme Generator</h1>
       </div>
-      <h2 className="header__project">React Course - Project 3</h2>
     </header>
   );
 }
